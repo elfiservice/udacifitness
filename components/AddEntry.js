445de82c59f257705422a57e546c271df4e23a34
Armando Jr <elfiservice@hotmail.com>
2018-11-20 08:58:42 -0300

    feat: provided incremente decrement slide functions

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -3,11 +3,47 @@ import { View } from 'react-native'
 import { getMetricMetaInfo } from '../utils/helpers'
 
  export default class AddEntry extends Component {
-  render() {
-    return (
-      <View>
-        {getMetricMetaInfo('run').getIcon()}
-      </View>
-    )
-  }
+    state = {
+        run: 0,
+        bike: 0,
+        swim: 0,
+        sleep: 0,
+        eat: 0,
+      }
+
+      increment = (metric) => {
+        const { max, step } = getMetricMetaInfo(metric)
+        this.setState((state) => {
+            const count = state[metric] + step
+            return {
+                ...state,
+                [metric]: count > max ? max : count,
+            }
+        })
+      }
+
+      decrement = (metric) => {
+        this.setState((state) => {
+            const { step } = getMetricMetaInfo(metric)
+            const count = state[metric] - step
+            return {
+                ...state,
+                [metric]: count < 0 ? 0 : count,
+            }
+        })
+      }
+
+      slide = (metric, value) => {
+        this.setState(() => ({
+            [metric]: value
+        }))
+      }
+
+    render() {
+        return (
+            <View>
+                {getMetricMetaInfo('run').getIcon()}
+            </View>
+        )
+    }
 } 
