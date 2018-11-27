09d6fbfd21055873c36beb23636c2fde2d46ce50
Armando Jr <elfiservice@hotmail.com>
2018-11-27 07:56:06 -0300

    feat: connected AddEntry compt and dispatch actions

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { View, TouchableOpacity, Text } from 'react-native'
-import { getMetricMetaInfo, timeToString } from '../utils/helpers'
+import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
 import { Ionicons } from '@expo/vector-icons'
 import TextButton from './TextButton'
 import { submitEntry, removeEntry } from '../utils/api'
+import { connect } from 'react-redux'
+import { addEntry } from '../actions'
 
 function SubmitBtn ({ onPress }) {
     return (
@@ -17,7 +19,7 @@ function SubmitBtn ({ onPress }) {
     )
 }
 
- export default class AddEntry extends Component {
+class AddEntry extends Component {
     state = {
         run: 0,
         bike: 0,
@@ -57,23 +59,29 @@ function SubmitBtn ({ onPress }) {
     submit = () => {
         const key = timeToString()
         const entry = this.state
-            // Update Redux
-            this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }))
-            // Navigate to home
+        // Update Redux
+        this.props.dispatch(addEntry({
+            [key]: entry
+        }))
+        this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }))
+        // Navigate to home
 
-            // Save to "DB"
-            submitEntry({ key, entry })
+        // Save to "DB"
+        submitEntry({ key, entry })
 
-            // Clear local notification
+        // Clear local notification
 
     }
 
     reset = () => {
         const key = timeToString()
-            // Update Redux
-            // Route to Home
-            // Update "DB"
-            removeEntry(key)
+        // Update Redux
+        this.props.dispatch(addEntry({
+            [key]: getDailyReminderValue()  //return a obj with a Prop .today
+        }))
+        // Route to Home
+        // Update "DB"
+        removeEntry(key)
     }      
 
     render() {
@@ -120,4 +128,13 @@ function SubmitBtn ({ onPress }) {
             </View>
         )
     }
-} 
+}
+
+function mapStateToProps (state) {
+    const key = timeToString()
+        return {
+        alreadyLogged: state[key] && typeof state[key].today === 'undefined'
+    }
+}
+
+export default connect(mapStateToProps)(AddEntry)
