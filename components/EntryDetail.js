13b395917194048514bafd7abfd58cc791525224
Armando Jr <elfiservice@hotmail.com>
2018-12-01 11:03:19 -0300

    feat: provided reset Btn in EntryDetail

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
+import { timeToString, getDailyReminderValue } from '../utils/helpers'
 import MetricCard from './MetricCard'
 import { white } from '../utils/colors'
+import TextButton from './TextButton'
+import { addEntry } from '../actions'
+import { removeEntry } from '../utils/api'
 
 class EntryDetail extends Component {
     static navigationOptions = ({ navigation }) => {
@@ -15,12 +19,25 @@ class EntryDetail extends Component {
         }
     }
 
+    reset = () => {
+        const { remove, goBack, entryId } = this.props
+        remove()
+        goBack()
+        removeEntry(entryId)
+    }
+    
+    shouldComponentUpdate (nextProps) {
+        return nextProps.metrics !== null && !nextProps.metrics.today
+    }
+
     render() {
         const { metrics } = this.props
         return (
             <View style={styles.container}>
                 <MetricCard metrics={metrics} />
-                <Text>Entry Detail - {JSON.stringify(this.props.navigation.state.params.entryId)}</Text>
+                <TextButton style={{margin: 20}} onPress={this.reset}>
+                    RESET
+                </TextButton>
             </View>
         )
     }
@@ -42,4 +59,16 @@ function mapStateToProps (state, { navigation }) {
     }
 }
 
-export default connect(mapStateToProps)(EntryDetail) 
+function mapDispatchToProps (dispatch, { navigation }) {
+    const { entryId } = navigation.state.params
+     return {
+      remove: () => dispatch(addEntry({
+        [entryId]: timeToString() === entryId
+          ? getDailyReminderValue()
+          : null
+      })),
+      goBack: () => navigation.goBack(),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EntryDetail) 
